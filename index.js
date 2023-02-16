6f894976fff73d890c6a49203f7c8355c5f312d7
Qluqlu3 <plksmcgof@gmail.com>
2023-02-17 01:52:18 +0900

    use post and patch

--- index.js
+++ index.js
@@ -2,8 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+let users = [];
+
 app.get('/', (req, res) => {
   res.send('Express.js');
+});
+
+app.post('/users', (req, res) => {
+  const { name, email } = req.body;
+  const user = { id: users.length + 1, name, email };
+  users.push(user);
+  res.status(201).json(user);
+});
+
+app.patch('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name, email } = req.body;
+  const user = users.find((user) => user.id === id);
+  if (user) {
+    user.name = name || user.name;
+    user.email = email || user.email;
+    res.json(user);
+  } else {
+    res.status(404).send('User not found');
+  }
 });
 
 app.listen(port, () => {
